d3d7876851732068b28c2c54998d48e3881d1438
Franco <francohormazabalgodoy@gmail.com>
2021-08-14 14:41:32 -0400

    Edition 3

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/graiphtag'));
+app.use(express.static(__dirname + '/dist/practiceTagger'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/graiphtag/index.html'));
+res.sendFile(path.join(__dirname+'/dist/practiceTagger/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
